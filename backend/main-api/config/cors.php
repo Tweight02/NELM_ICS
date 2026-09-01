3441919b44a37dfb54d9d261669402939ce37635
Tweight02 <edistonmarc@gmail.com>
2026-09-01 17:59:16 +0800

    fix: update CORS paths to allow all routes

--- backend/main-api/config/cors.php
+++ backend/main-api/config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'],
+    'paths' => ['*', 'sanctum/csrf-cookie', 'login', 'logout'],
 
     'allowed_methods' => ['*'],
 
