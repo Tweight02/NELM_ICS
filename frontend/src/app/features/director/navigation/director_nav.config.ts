43bb81889a520f041fc6ca726062e066a5210c31
Tweight02 <edistonmarc@gmail.com>
2026-09-06 19:35:39 +0800

    feat: add navigation items for Director role

---
+++ frontend/src/app/features/director/navigation/director_nav.config.ts
@@ -0,0 +1,5 @@
+import { NavItem } from "../../../core/models/navigation/nav-item.model";
+
+export const director_nav_items: NavItem[] = [
+    { label: 'Dashboard', path: '/director/home', icon: 'fa-house', roles: ['director'] },
+];
